0a50d352e2c4f0d2601edf31e1920ee201166a23
fishfoldyy <145004951+fishfoldyy@users.noreply.github.com>
2025-12-05 10:05:43 +0000

    minor UI fix

--- public/squad/script.js
+++ public/squad/script.js
@@ -87,7 +87,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             } else if (pos === "Midfielder" || pos === "Forward") { // MID & FWD best stat
                 if (goals > 0 && assts > 0) {
-                    shownStats = `${goals} goals and ${assts} assists`;
+                    shownStats = `${goals} goals and ${assts} assist(s)`;
                 } else if (goals > 0) {
                     shownStats = `${goals} goal(s)`;
                 } else if (assts > 0) {
